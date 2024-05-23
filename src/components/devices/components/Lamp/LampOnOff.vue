2b6e669d746bdc704f9808165d3d0f33122c6145
Maggie Taurian <mtaurian@itba.edu.ar>
2024-05-22 23:23:19 -0300

    created LampOnOff.vue

---
+++ src/components/devices/components/Lamp/LampOnOff.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="power-button">
+    <v-btn
+      @click="handleOnClick"
+      class="ma-2"
+      :color="isOn ? 'red-lighten-2' : 'green-lighten-2'"
+      :size="50"
+      variant="text"
+    >
+      <v-icon icon="mdi-power" :size="50" />
+    </v-btn>
+    <v-label>{{ isOn ? 'On' : 'Off' }}</v-label>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { type ApiSong, execute_device_action } from '@/api'
+
+const props = defineProps<{
+  state: { result: Record<string, string | number | boolean | ApiSong[] | null> }
+  device_id: string
+}>()
+
+enum PowerState {
+  ON = 'on',
+  OFF = 'off'
+}
+
+const isOn = computed(() => {
+  return props.state.result.status === PowerState.ON
+})
+
+const emit = defineEmits(['power-changed'])
+
+/**
+ * If lamp is on, change color.
+ * Else informs how to proceed.
+ */
+const handleOnClick = async () => {
+  if (isOn.value) {
+    await execute_device_action(props.device_id, 'turnOff', [])
+    emit('power-changed')
+  } else {
+    await execute_device_action(props.device_id, 'turnOn', [])
+    emit('power-changed')
+  }
+}
+</script>
