2ebd1db00d72b3dc0d592b4a3d79de22fc1374af
Maggie Taurian <mtaurian@itba.edu.ar>
2024-05-23 21:14:36 -0300

    modifications in LampColorPicker.vue

--- src/components/devices/components/Lamp/LampColorPicker.vue
+++ src/components/devices/components/Lamp/LampColorPicker.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="color-picker">
-    <v-color-picker :model-value="currentColor" @update:model-value="handleOnChange" mode="rgba">
+    <v-color-picker :model-value="currentColor"
+                    @update:model-value="handleOnChange" mode="rgba"
+                    canvas-height="80"
+                    width="300">
     </v-color-picker>
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue'
-import { type ApiSong, execute_device_action } from '@/api'
+import { type ApiReturns, execute_device_action } from '@/api'
 
 const props = defineProps<{
-  state: { result: Record<string, string | number | boolean | ApiSong[] | null> }
+  state: { result: Record<string, ApiReturns> }
   device_id: string
 }>()
 
@@ -34,12 +37,9 @@ const emit = defineEmits(['color-changed'])
 const handleOnChange = async (newColor: string) => {
   newColor = newColor.slice(1)
   currentColor.value = newColor
-  console.log(newColor)
-  if (!isOn.value) {
+  if (isOn.value) {
     await execute_device_action(props.device_id, 'setColor', [newColor])
     emit('color-changed')
-  } else {
-    alert('Turn lamp on to change color')
   }
 }
 </script>
@@ -48,7 +48,5 @@ const handleOnChange = async (newColor: string) => {
 .color-picker {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  justify-items: center;
 }
 </style>
