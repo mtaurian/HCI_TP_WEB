295744026c2ec9106da4fafd87bbf83911f81a04
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-05-25 00:55:05 -0300

    Unused import

--- src/components/ControllerPlaceholder.vue
+++ src/components/ControllerPlaceholder.vue
@@ -16,7 +16,6 @@
 
 import { useHomeStore, useRoomStore, useDeviceStore } from '@/stores'
 import { ref } from 'vue'
-import AddHome from './AddHome.vue'
 import BlindController from '@/components/devices/BlindController.vue'
 import FaucetController from '@/components/devices/FaucetController.vue'
 import LampController from '@/components/devices/LampController.vue'
