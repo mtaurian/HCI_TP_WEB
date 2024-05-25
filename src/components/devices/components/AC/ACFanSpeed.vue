0e644762830f6ac707bde27fd5b01ae413e9e021
Maggie Taurian <mtaurian@itba.edu.ar>
2024-05-25 18:34:58 -0300

    created AC fan speed functions

---
+++ src/components/devices/components/AC/ACFanSpeed.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="title">
+    <p class="font-weight-black" size="70">Fan Speed</p>
+  </div>
+  <v-col class="d-flex justify-space-around">
+    <v-btn-toggle
+      v-model="fanSpeed"
+      color="deep-purple-accent-3"
+      rounded="0"
+      :disabled="isAuto"
+      @update:model-value="handleOnClick"
+      group
+    >
+      <v-btn value="25">
+        25
+      </v-btn>
+
+      <v-btn value="50">
+        50
+      </v-btn>
+
+      <v-btn value="75">
+        75
+      </v-btn>
+
+      <v-btn value="100">
+        100
+      </v-btn>
+    </v-btn-toggle>
+    </v-col>
+    <v-col>
+      <v-switch label="Auto" inset color="primary" v-model="auto" @update:model-value="handleOnClick('auto')"/>
+    </v-col>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { type ApiReturns, execute_device_action } from '@/api'
+
+const props = defineProps<{
+  state: { result: Record<string, ApiReturns> }
+  device_id: string
+}>()
+
+const fanSpeed = ref(props.state.result.fanSpeed as string)
+const auto = ref(props.state.result.fanSpeed === 'auto')
+
+const isAuto = computed(() => auto.value)
+
+const emit = defineEmits(['speed-changed'])
+
+const handleOnClick = async (newSpeed: string) => {
+  await execute_device_action(props.device_id, 'setFanSpeed', [newSpeed])
+  emit('speed-changed')
+}
+</script>
+
+<style scoped>
+.title{
+  margin-bottom: 10px;
+}
+.button{
+  display : flex;
+  margin-right: 20px;
+  flex-direction: column;
+  justify-content: center;
+  justify-items: center;
+}
+
+</style>
