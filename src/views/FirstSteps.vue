c08b8406976882fc4b4a00de3e978aa645bbf4cf
diegobadin <dbadin@itba.edu.ar>
2024-05-26 05:13:01 -0300

    FirstSteps implementacion

---
+++ src/views/FirstSteps.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AddHome from '@/components/AddHome.vue'
+</script>
+
+<template>
+  <main>
+    <AddHome mandatory></AddHome>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
